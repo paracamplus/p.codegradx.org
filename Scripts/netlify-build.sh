428d113eff729b21c0d5cd8b67f0e9621bd1e78d
Christian Queinnec <Christian.Queinnec@gmail.com>
2021-01-11 10:29:17 +0100

    20210111_092917

--- Scripts/netlify-build.sh
+++ Scripts/netlify-build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# Time-stamp: "2021-01-11 09:55:20 queinnec"
+# Time-stamp: "2021-01-11 10:28:00 queinnec"
 
 # Build the server on Netlify
 export CONTEXT=production
@@ -7,17 +7,18 @@ export NODE_ENV=$CONTEXT
 export NODE_VERSION=12.x
 
 #ls -Ral
-whoami
-pwd
+echo "*** whoami -> $(whoami)"
+echo "*** pwd    -> $(pwd)"
+echo "*** printenv is"
 printenv
 
 mkdir -p secrets/
 
-( cd secrets
-  if [ ! -f fkeyPrivate ]
+( cd secrets/
+  if [ ! -f ./fkeyPrivate ]
   then 
       ssh-keygen -t rsa -N '' -b 1024 -C "fkey@f.codegradx.org" -f ./fkeyPrivate
-      openssl rsa -in fkeyPrivate -pubout -out fkeyPublic
+      openssl rsa -in ./fkeyPrivate -pubout -out ./fkeyPublic
   fi
 )
 
