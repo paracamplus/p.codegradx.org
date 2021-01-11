cf377d492b29674db153fbf358f25136d8f5cd51
Christian Queinnec <Christian.Queinnec@gmail.com>
2021-01-11 09:55:40 +0100

    20210111_085540

--- Scripts/netlify-build.sh
+++ Scripts/netlify-build.sh
@@ -1,21 +1,24 @@
 #! /bin/bash
-# Time-stamp: "2021-01-11 09:47:15 queinnec"
+# Time-stamp: "2021-01-11 09:55:20 queinnec"
 
 # Build the server on Netlify
 export CONTEXT=production
 export NODE_ENV=$CONTEXT
 export NODE_VERSION=12.x
 
-ls -Ral
+#ls -Ral
 whoami
 pwd
+printenv
 
 mkdir -p secrets/
 
 ( cd secrets
-  rm -f fkeyPublic fkeyPrivate
-  ssh-keygen -t rsa -N '' -b 1024 -C "fkey@f.codegradx.org" -f ./fkeyPrivate
-  openssl rsa -in fkeyPrivate -pubout -out fkeyPublic
+  if [ ! -f fkeyPrivate ]
+  then 
+      ssh-keygen -t rsa -N '' -b 1024 -C "fkey@f.codegradx.org" -f ./fkeyPrivate
+      openssl rsa -in fkeyPrivate -pubout -out fkeyPublic
+  fi
 )
 
 # Erase some previous cached files:
