e36c65dd1c08fa6428494179bff701ec4b3cbfaf
Christian Queinnec <Christian.Queinnec@gmail.com>
2021-01-19 18:13:24 +0100

    20210119_171324

--- api/signupinit.json.js
+++ api/signupinit.json.js
@@ -5,11 +5,16 @@
    That directory also contains ./{src,static,node_modules}/
 */
 
-import { get } from '/var/task/api/src/routes/api/signupinit.json.js';
+//import { get } from '/var/task/api/src/routes/api/signupinit.json.js';
 import { VercelRequest, VercelResponse } from './vercel.mjs';
 
 async function handler (req, res) {
 
+    let info = 'Content of .: ';
+    info += fs.readdirSync('./').join(', ');
+    info += '\n\n';
+    console.log(info);
+    
     let info = 'Content of /var/task: ';
     info += fs.readdirSync('/var/task/').join(', ');
     info += '\n\n';
@@ -23,7 +28,7 @@ async function handler (req, res) {
     try {
         const wrappedreq = new VercelRequest(req);
         const wrappedres = new VercelResponse(res);
-        await get(wrappedreq, wrappedres);
+        //await get(wrappedreq, wrappedres);
     } catch (exc) {
         console.error(`error in signupinit.json.js handler`, {exc});
         res.status(500);
