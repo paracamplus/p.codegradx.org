1d43f9854e8284cd8b0f8b84b18576c7c378242b
Christian Queinnec <Christian.Queinnec@gmail.com>
2021-01-19 09:57:29 +0100

    20210119_085729

--- Scripts/vercel-build.sh
+++ Scripts/vercel-build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# Time-stamp: "2021-01-18 18:56:01 queinnec"
+# Time-stamp: "2021-01-19 09:46:21 queinnec"
 
 # Build the P server on Vercel:
 
@@ -22,17 +22,26 @@ then
     printenv
 fi
 
-echo "*** Building pair of keys:"
-mkdir -p secrets/
-( cd secrets/
-  if [ ! -f ./fkeyPrivate ]
-  then 
-      ssh-keygen -t rsa -N '' -b 1024 -C "fkey@f.codegradx.org" -f ./fkeyPrivate
-      openssl rsa -in ./fkeyPrivate -pubout -out ./fkeyPublic
-  fi
-  ls -l ../secrets/
-)
-cp -rp secrets api/
+if [ -z "$FKEY_PUBLIC" ]
+then if [ -z "$FKEY_PRIVATE" ]
+     then
+         echo "*** Building pair of keys:"
+         mkdir -p secrets/
+         ( cd secrets/
+           if [ ! -f ./fkeyPrivate ]
+           then 
+               ssh-keygen -t rsa -N '' -b 1024 -C "fkey@f.codegradx.org" -f ./fkeyPrivate
+               openssl rsa -in ./fkeyPrivate -pubout -out ./fkeyPublic
+           fi
+           ls -l ../secrets/
+         )
+         cp -rp secrets api/
+     else
+         unset FKEY_PUBLIC
+     fi
+else
+    unset FKEY_PRIVATE
+fi
 
 echo "*** Building the p.codegradx.org server..."
 npm run export
@@ -41,6 +50,6 @@ echo "*** Building the API functions..."
 cp -rp src static api/
 #sed -i.bak api/src/server.js -e 's/polka/export default polka/'
 #rm -f api/src/server.js.bak
-(cd api/ && npm install && npm run build)
+(cd api/ && npm ci && npm run build)
 
 # end of vercel-build.sh
